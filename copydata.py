1ee3e782a608069bb07c9645e67920a57a3eb9d9
deafcon01 <deafcon01@gmai.com>
2019-04-07 23:26:12 +0530

    copy

--- copydata.py
+++ copydata.py
@@ -1,6 +1,7 @@
 import os
 from shutil import copyfile
 import pdb
+import re
 train_list=['27-03-2019 (mar)','26-03-2019.. (mar)','01-04-2019']
 test_list=['02-04-2019']
 def cpy(lists,f):
@@ -9,9 +10,9 @@ def cpy(lists,f):
         for files in os.listdir(folder):
             #pdb.set_trace()
             if files.find('.jpg') > 0:
-                copyfile(os.path.join(path,folder,files),os.path.join(path,'data',f,'img',files))#.replace(' ','_')))
+                copyfile(os.path.join(path,folder,files),os.path.join(path,'data',f,'img',re.sub(' +','_',files)))#.replace(' ','_')))
             elif files.find('.json') > 0:
-                copyfile(os.path.join(path,folder,files),os.path.join(path,'data',f,'json',files))#.replace(' ','_')))
+                copyfile(os.path.join(path,folder,files),os.path.join(path,'data',f,'json',re.sub(' +','_',files)))#.replace(' ','_')))
                 
 
 
