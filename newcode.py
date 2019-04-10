639148b27628640762b5231c515a27fdeefe8bc4
deafcon01 <deafcon01@gmai.com>
2019-04-10 07:45:51 +0530

    master

---
+++ newcode.py
@@ -0,0 +1,134 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+from PIL import Image
+import cv2
+from shutil import copyfile
+import pdb
+import re
+from json2xml import json2xml,readfromjson
+
+
+train_list=['27-03-2019 (mar)','26-03-2019.. (mar)','01-04-2019']
+test_list=['02-04-2019']
+
+def cpy(lists,f):
+    path =os.getcwd()
+    for folder in lists:
+        for files in os.listdir(folder):
+            #pdb.set_trace()
+            if files.find('.jpg') > 0:
+                copyfile(os.path.join(path,folder,files),os.path.join(path,'data',f,'img',re.sub(r"[^\S\n\t]+",'_',files)))#.replace(' ','_')))
+            elif files.find('.json') > 0:
+                copyfile(os.path.join(path,folder,files),os.path.join(path,'data',f,'json',re.sub(r"[^\S\n\t]+",'_',files)))#.replace(' ','_')))
+
+
+
+def clahe():
+    cdir=os.getcwd()
+    outdir='images/'
+    for directory in ['data/train','data/test']:
+        #pdb.set_trace()
+        if not os.path.exists(os.path.join(cdir,directory,outdir)):
+            os.mkdir('{}/{}/{}'.format(cdir,directory,outdir))
+        for files in os.listdir(os.path.join(cdir,directory,"img")):
+            bgr = cv2.imread(os.path.join(cdir,directory,"img",files))
+            lab = cv2.cvtColor(bgr,cv2.COLOR_BGR2LAB)
+            lab_planes = cv2.split(lab)
+            clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(16,16))
+            lab_planes[0]=clahe.apply(lab_planes[0])
+            lab = cv2.merge(lab_planes)
+            bgr = cv2.cvtColor(lab,cv2.COLOR_LAB2BGR)
+            cv2.imwrite(os.path.join(cdir,directory,outdir,files),bgr)
+
+def jsontoxml():
+    cdir=os.getcwd()
+    outdir='annotations'
+    for directory in ['data/train','data/test']:
+        if not os.path.exists(os.path.join(cdir,directory,outdir)):
+            os.mkdir('{}/{}/{}'.format(cdir,directory,outdir))
+            #pdb.set_trace()
+        for files in os.listdir(os.path.join(cdir,directory,'json')):
+            if files.endswith('.json'):
+                data=readfromjson(os.path.join(cdir,directory,'json',files))
+                xml=json2xml.Json2xml(data).to_xml()
+                name=files.replace('.json','.xml')
+                txt_file= open(os.path.join(cdir,directory,outdir,name),'w')
+                txt_file.write(xml)
+                txt_file.close()
+
+
+def xml_to_csv(path):
+    xml_list = []
+    ann_path = path.replace("images","annotations")
+    for xml_file in os.listdir(ann_path):
+        #print(xml_file)
+        tree = ET.parse(os.path.join(ann_path,xml_file))
+        root = tree.getroot()
+        if root.find('object'):
+            #print('found object')
+            for member in root.findall('object'):
+                #fname = root.find('filename').text
+                #fname.replace(' ','_')
+                value = (root.find('filename').text,
+                        int(root.find('size')[0].text),
+                        int(root.find('size')[1].text),
+                        member[0].text,
+                        int(member[4][0].text),
+                        int(member[4][1].text),
+                        int(member[4][2].text),
+                        int(member[4][3].text)
+                        ) 
+                xml_list.append(value)
+        elif root.find('annotations'):
+            filename= xml_file.replace('.xml','.jpg')
+            im = Image.open(os.path.join(path,filename))
+            w,h=im.size
+            im.close()
+            for member in root.findall('annotations'):
+                #fname = root.find('meta/imageid').text
+                #print(fname)
+                #fname.replace(' ','_')
+                value = (root.find('meta/imageid').text,
+                        int(w),
+                        int(h),
+                        member[1].text,
+                        float(member[3].text),
+                        float(member[5].text),
+                        float(member[4].text),
+                        float(member[0].text)
+                        )
+                xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+def underscore():
+    filenames = ['data/train_labels.csv','data/test_labels.csv']
+    for filename in filenames:
+        with open(filename,'r') as f:
+            lines = f.readlines()
+        #lines = [line.replace(' ','_') for line in lines]
+        #lines = [line.replace('__','_') for line in lines]
+        lines = [re.sub(r"[^\S\n\t]+",'_',line) for line in lines]
+        with open(filename,'w') as f:
+            f.writelines(lines)
+
+def main():
+    train_list=['27-03-2019 (mar)','26-03-2019.. (mar)','01-04-2019']
+    test_list=['02-04-2019']
+    #pdb.set_trace()
+    cpy(train_list,'train')
+    cpy(test_list,'test')
+    clahe()
+    jsontoxml()
+    for directory in ['data/train','data/test']:
+        image_path = os.path.join(os.getcwd(), '{}/images'.format(directory))
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv('{}_labels.csv'.format(directory), index=None)
+        print('Successfully converted xml to csv.')
+    underscore()
+
+if __name__ == "__main__":
+    main()
--- underscore.py
+++ underscore.py
@@ -1,8 +1,10 @@
+import re
 filenames = ['data/train_labels.csv','data/test_labels.csv']
 for filename in filenames:
     with open(filename,'r') as f:
         lines = f.readlines()
-    lines = [line.replace(' ','_') for line in lines]
-    lines = [line.replace('__','_') for line in lines]
+    #lines = [line.replace(' ','_') for line in lines]
+    #lines = [line.replace('__','_') for line in lines]
+    lines = [re.sub(r"[^\S\n\t]+",'_',line) for line in lines]
     with open(filename,'w') as f:
         f.writelines(lines)
